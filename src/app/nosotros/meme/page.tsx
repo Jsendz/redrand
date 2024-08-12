6cfbc50777fa2fba7ae0fd65f385ec80534a553e
jsendz <joshuasendzul@gmail.com>
2024-08-12 23:00:13 +0200

    redrand

--- src/app/nosotros/meme/page.tsx
+++ src/app/nosotros/meme/page.tsx
@@ -2,9 +2,8 @@ export default function Meme() {
     return(
 
 <div className="w-full h-full md:h-screen">
-        <iframe
-        src="https://www.youtube.com/watch?v=9o8qf7anq0g"
-    />
+
+
     </div>
 
 )
